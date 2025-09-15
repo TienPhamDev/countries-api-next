2b13a63b52999e3d60362740237032b39255d741
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-09-15 15:24:22 -0400

    Update page.js

--- src/app/country/[name]/page.js
+++ src/app/country/[name]/page.js
@@ -1,9 +1,16 @@
 import BackButton from "@/components/backbutton/BackButton";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 const DetailsCountryPage = async ({ params }) => {
   const countryName = await params.name;
   const res = await fetch(
-    `https://restcountries.com/v3.1/name/${countryName}?fullText=true`,
+    `https://restcountries.com/v3.1/name/${countryName}`,
     {
       method: "GET",
       cache: "force-cache",
@@ -11,12 +18,13 @@ const DetailsCountryPage = async ({ params }) => {
   );
   const data = await res.json();
   console.log(data);
+  const border = data[0].borders;
+  console.log(border);
   return (
     <section className="px-12">
       <BackButton />
-      {data[0].name.official}
-      <div className="h-72">
-        <div id="flags" className="relative w-full h-full">
+      <div className="h-72  flex justify-between items-center flex-col md:flex-row">
+        <div id="flags" className="relative w-[45%] h-full">
           <Image
             src={`${data[0].flags.svg}`}
             alt="flags"
@@ -25,7 +33,20 @@ const DetailsCountryPage = async ({ params }) => {
             className="rounded-[5px]"
           />
         </div>
-        <div id="country-details"></div>
+        <Card id="country-details" className="w-[45%]">
+          <CardHeader>
+            <CardTitle className="font-bold text-xl">
+              {data[0].name.official}
+            </CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+          <CardFooter>
+            <span>
+              Border :{" "}
+              {border === undefined ? "No border with any country." : border}
+            </span>
+          </CardFooter>
+        </Card>
       </div>
     </section>
   );
