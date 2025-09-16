acfbbf44d4d16f6272e028b3c8be0597423af057
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-09-16 12:45:28 -0400

    Update MainComponent.js
    set the fetched data to redux through MainComponent

--- src/components/main/MainComponent.js
+++ src/components/main/MainComponent.js
@@ -5,16 +5,29 @@ import FilterByRegion from "@/components/filterbyregion/FilterByRegion";
 import PaginationPage from "@/components/paginationpage/PaginationPage";
 import CountryList from "@/components/countrylist/CountryList";
 import { PaginationPageProvider } from "@/context/paginationPageContext";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useRef } from "react";
+import { setCountries } from "@/redux/slices/countries.slice";
 const MainComponent = ({ data }) => {
+  const dispatch = useAppDispatch();
+  const initialized = useRef(false);
+  if (!initialized.current) {
+    dispatch(setCountries(data));
+    initialized.current = true;
+  }
+
+  const countriesData = useAppSelector(
+    (state) => state.countries.countriesRedux
+  );
   return (
     <main className="py-10 md:px-12 px-2">
       <section className="flex justify-between flex-col md:flex-row gap-4">
         <Search />
         <FilterByRegion />
       </section>
-      <PaginationPageProvider data={data}>
+      <PaginationPageProvider data={countriesData}>
         <CountryList />
-        <PaginationPage data={data} />
+        <PaginationPage data={countriesData} />
       </PaginationPageProvider>
     </main>
   );
